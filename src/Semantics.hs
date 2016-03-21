22b679efcd1bae825d3ac861597e6837c0f7bbe4
t00n <kelkununtel@hotmail.com>
2016-03-21 20:20:50 +0100

    [add] check declarations in return

--- src/Semantics.hs
+++ src/Semantics.hs
@@ -62,6 +62,7 @@ checkStatement stmt st =
         If e stmt1 -> checkExpression e st >> checkStatement stmt1 st
         IfElse e stmt1 stmt2 -> checkExpression e st >> checkStatement stmt1 st >> checkStatement stmt2 st
         While e stmt1 -> checkExpression e st >> checkStatement stmt1 st
+        Return e -> checkExpression e st
         Block _ [] -> Right st
         Block decl stmts -> walkProgram decl st >> checkStatements stmts st
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -171,4 +171,9 @@ main = hspec $ do
             checkSemantics ast `shouldBe` Left (SemanticError NotDeclaredError "a")
             let ast2 = scan_and_parse "int a = 5; int tiny() { while (a) a = 5; }"
             checkSemantics ast2 `shouldBe` Right ()
+        it "Checks that variables are declared before use in return" $ do
+            let ast = scan_and_parse "int tiny() { return a; }"
+            checkSemantics ast `shouldBe` Left (SemanticError NotDeclaredError "a")
+            let ast2 = scan_and_parse "int a = 5; int tiny() { return a; }"
+            checkSemantics ast2 `shouldBe` Right ()
 
