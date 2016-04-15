3891bae7db5c0ba981915cc716c002cff4ff1b16
t00n <kelkununtel@hotmail.com>
2016-04-15 14:42:04 +0200

    [enh] useless change

--- src/Utility.hs
+++ src/Utility.hs
@@ -1,5 +1,5 @@
 module Utility where
 
 -- composition operator for 2 args functions
-(...) :: (Functor f, Functor f1) => (a -> b) -> f (f1 a) -> f (f1 b)
-(...) = fmap . fmap
+(...) ::  (c -> d) -> (a -> b -> c) -> (a -> b -> d)
+(...) = (.) . (.)
