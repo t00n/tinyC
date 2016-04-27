d411d87b3a44c36b0299e967674326123e66b8d2
t00n <kelkununtel@hotmail.com>
2016-04-27 19:04:11 +0200

    [add] NASM : push instructions

--- src/NASMGenerator.hs
+++ src/NASMGenerator.hs
@@ -9,13 +9,18 @@ data NASMInstruction = MOV1 RegisterSize RegisterName RegisterName
                      | MOV2 Register Address
                      | MOV3 Address Register
                      | MOV4 Register Int
-                     | MOV5 AddressSize Address Int
+                     | MOV5 AddressSize Address Constant
+                     | PUSH1 RegisterName
+                     | PUSH2 Address
+                     | PUSH3 Constant
 
 data RegisterSize = LSB | MSB | WORD | DWORD
 
 data RegisterName = A | B | C | D | SI | DI | SP | BP
 
 data Register = Register RegisterName RegisterSize
+
+type Constant = Int
 
 type Multiplier = Int
 
